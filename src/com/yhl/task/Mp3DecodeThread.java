78cb10a77836e5ffb0806d9d83778645845ebbca
yuanhonglie <604221320@qq.com>
2014-08-20 23:38:02 +0800

    play pcm data using AudioTrack
    使用AduioTrack播放pcm数据

--- src/com/yhl/task/Mp3DecodeThread.java
+++ src/com/yhl/task/Mp3DecodeThread.java
@@ -1,13 +1,8 @@
 package com.yhl.task;
 
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.Arrays;
-
-import android.os.Environment;
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioTrack;
 import android.util.Log;
 
 import com.yhl.jni.Libmad;
@@ -18,63 +13,79 @@ public class Mp3DecodeThread extends Thread {
 	private static final String TAG = Mp3DecodeThread.class.getSimpleName();
 	private int mHandle = -1;
 	private static final int BUFFER_SIZE_SHORT = 4096;
-	private static final int BUFFER_SIZE_BYTE = BUFFER_SIZE_SHORT + BUFFER_SIZE_SHORT;
 	private short [] mBuffer = new short[BUFFER_SIZE_SHORT];
-	private String outPath = Environment.getExternalStorageDirectory().getPath() + "/test.pcm";
 	private boolean inited = false;
 	private Mp3Info mInfo = new Mp3Info();
 	
-	private AudioBuffer [] mAudioBuffers = new AudioBuffer[2];
-	private int flag;
+	private AudioTrack mAudioTrack;
 	
 	public Mp3DecodeThread(String fileName) {
 		mHandle = Libmad.openFile(fileName);
 		inited = false;
-		flag = 0;
 	}
-	
+
 	@Override
 	public synchronized void run() {
 		super.run();
-		
+
 		int size = 0;
-		BufferedOutputStream bos = null;
-		try {
-			File pcmFile = new File(outPath);
-			FileOutputStream fos = new FileOutputStream(pcmFile);
-			bos = new BufferedOutputStream(fos);
-			while ((size = Libmad.readSamplesInShortBuffer(mHandle, mBuffer, BUFFER_SIZE_SHORT)) > 0) {
-				if (!inited) {
-					mInfo.sampleRate = Libmad.getSampleRate(mHandle);
-					mInfo.durationTime = Libmad.getDurationTime(mHandle);
-					mInfo.mode = Libmad.getMode(mHandle);
-					Log.i(TAG, "Mp3Info = " + mInfo);
-					inited = true;
-				}
-				
-				if (bos != null) {
-					byte [] buffer = shortArray2ByteArray(mBuffer, size);
-					try {
-						bos.write(buffer);
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-				}
-				
-				try {
-					this.wait();
-				} catch (InterruptedException e1) {
-					e1.printStackTrace();
-				}
+		while ((size = Libmad.readSamplesInShortBuffer(mHandle, mBuffer,
+				BUFFER_SIZE_SHORT)) > 0) {
+			if (!inited) {
+				mInfo.sampleRate = Libmad.getSampleRate(mHandle);
+				mInfo.durationTime = Libmad.getDurationTime(mHandle);
+				mInfo.mode = Libmad.getMode(mHandle);
+				Log.i(TAG, "Mp3Info = " + mInfo);
+				inited = true;
+				int bufferSizeInBytes = AudioTrack.getMinBufferSize(mInfo.sampleRate, AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT);
+				mAudioTrack = new AudioTrack(AudioManager.STREAM_MUSIC, mInfo.sampleRate,
+						AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT, bufferSizeInBytes*4, AudioTrack.MODE_STREAM);
+				mAudioTrack.play();
 			}
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} finally {
+			
+			writeBuffer(mBuffer, size);
+			
 			try {
-				bos.close();
-			} catch (IOException e) {
+				sleep(20);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
+		}
+	}
+
+	
+	public void audioPlay() {
+		Log.i(TAG, "audioPlay 1");
+		if (mAudioTrack != null) {
+			Log.i(TAG, "audioPlay 2");
+			mAudioTrack.play();
+		}
+		Log.i(TAG, "audioPlay 3");
+	}
+	
+	public void audioPause() {
+		if (mAudioTrack != null) {
+			mAudioTrack.pause();
+		}
+	}
+	
+	
+	public void audioStop() {
+		if (mAudioTrack != null) {
+			mAudioTrack.stop();
+		}
+	}
+	
+	public void audioRelease() {
+		if (mAudioTrack != null) {
+			mAudioTrack.release();
+		}
+	}
+
+	private void writeBuffer(short [] buffer, int size) {
+		if (mAudioTrack != null) {
+			mAudioTrack.write(mBuffer, 0, size);
 		}
 	}
 	
@@ -91,9 +102,7 @@ public class Mp3DecodeThread extends Thread {
 
 		len = Math.min(len, size);
 		
-		int index = (flag + 1) % 2;
-		mAudioBuffers[index].size = len + len;
-		byte [] bytes = mAudioBuffers[index].getWriteBuffer();
+		byte [] bytes = new byte[len+len];
 		
 		for (int i = 0; i < len; i++) {
 			
@@ -105,25 +114,5 @@ public class Mp3DecodeThread extends Thread {
 		}
 		
 		return bytes;
-	}
-	
-	public byte [] read() {
-		flag += 1;
-		flag %= 2;
-		this.notify();
-		return mAudioBuffers[flag].getReadBuffer();
-	}
-	
-	public static class AudioBuffer {
-		byte [] buffer = new byte[BUFFER_SIZE_BYTE];
-		int size;
-		
-		public byte[] getReadBuffer() {
-			return Arrays.copyOfRange(buffer, 0, size);
-		}
-		
-		public byte[] getWriteBuffer() {
-			return buffer;
-		}
 	}
 }
